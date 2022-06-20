92327c05d12533fc3ed7db5486c3000eadeb44a1
devamorim <jadissonamorim@gmail.com>
2022-06-19 22:24:08 -0300

    subindo projeto

---
+++ Camundongo.cs
@@ -0,0 +1,24 @@
+namespace EntityCamundogos;
+    class Camundongo{
+        public string Name {get; set;}
+        public int Distancia {get; set;}
+        public int Colocação {get; set;}
+
+        public Camundongo(string name, int distancia, int colocação){
+            Name = name;
+            Distancia = distancia;
+            Colocação = colocação;
+        }
+        public void Mover(){
+            Random rd = new Random();
+            int rand_num = rd.Next(0, 10);
+            Distancia += rand_num;
+            if(Distancia > 100){
+                Distancia = 100;
+            }
+        }
+        public override string ToString()
+    {
+        return Name + " : " + Distancia;
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EntityCamundogos;
+using System.Linq;
+namespace Course;
+    class Program{
+        static void Main(string[] args) {
+            int posicao = 1;
+            List<Camundongo> camundongos = new List<Camundongo>(){
+                new Camundongo("Camundongo A", 0, 0),
+                new Camundongo("Camundongo B", 0, 0),
+                new Camundongo("Camundongo C", 0, 0),
+                new Camundongo("Camundongo D", 0, 0)
+            };
+            var r1 = camundongos.Where(p => p.Colocação == 0).FirstOrDefault();
+            Console.WriteLine(r1 + "hehe");
+            while(r1 != null){
+                Console.WriteLine();
+                foreach(Camundongo camun in camundongos){
+                camun.Mover();
+                Console.WriteLine(camun.ToString());
+                if(camun.Distancia == 100 && camun.Colocação == 0){
+                    camun.Colocação = posicao;
+                    posicao++;
+                }
+                }   
+                Console.WriteLine();
+                r1 = camundongos.Where(p => p.Colocação == 0).FirstOrDefault();
+                Console.ReadKey();
+            }
+            var ranking = camundongos.OrderBy(p => p.Colocação);
+            foreach(Camundongo camun in ranking){
+                Console.WriteLine(camun.Colocação + "º - " + camun.Name);
+            }            
+        }
+    }    
+    
+
